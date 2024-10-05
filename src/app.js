d533cf1dd49381dc68cba3a044457c2c1e15eeca
Shubham Jangle <jangleshubham13@gmail.com>
2024-10-05 17:20:42 +0530

    feat: helmet, compression, loggers, morgan

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
+const morgan = require("morgan");
+const helmet = require("helmet");
 const express = require("express");
 const bodyParser = require("body-parser");
+const compression = require("compression");
+
 const appRoutes = require("./routes/appRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
 const limiter = require("./utils/rateLimiter.js");
@@ -8,11 +12,11 @@ const { notFound, errorHandler } = require("./utils/errorHandler.js");
 
 const app = express();
 
-// Use CORS
-app.use(corsOptions);
-
-// Use Rate Limiter
-app.use(limiter);
+app.use(corsOptions); // Use CORS protection
+app.use(limiter); // Request Rate Limiter
+app.use(helmet()); // Secures your Express app by setting various HTTP headers.
+app.use(morgan("dev")); // Logs HTTP requests for easier debugging and monitoring.
+app.use(compression()); // Reduces the size of the response body, improving performance.
 
 // Middlewares
 app.use(bodyParser.json());
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const app = require("./app");
 const { PrismaClient } = require("@prisma/client");
+const logger = require("./utils/logger");
 
 const prisma = new PrismaClient();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT}`);
 });
 
 // Close Prisma connection when the app shuts down
